68e90e25ae6cb82e0ad39d192c1a6e4c64a9d1e8
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-26 23:35:54 +0200

    Add header and footer components with styles and templates

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../views/Home/home.component';
+import { FooterComponent } from './../views/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
-templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent, FooterComponent],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
