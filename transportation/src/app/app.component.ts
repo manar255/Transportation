4f6a038e791364cddcbd4b85f9476fda23d77c50
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-26 22:25:06 +0200

    Add home section [ hero - feature - options ]

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './../views/Home/components/hero-section/hero-section.component';
+import { FeatureSectionComponent } from '../views/Home/components/feature-section/feature-section.component';
+import { OptionSectionComponent } from '../views/Home/components/option-section/option-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeroSectionComponent],
+  imports: [RouterOutlet,HeroSectionComponent,FeatureSectionComponent,OptionSectionComponent],
 templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ transportation/src/views/Home/components/option-section/option-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-option-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './option-section.component.html',
+  styleUrl: './option-section.component.css'
+})
+export class OptionSectionComponent {
+
+}
