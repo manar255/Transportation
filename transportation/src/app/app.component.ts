178ff036dbc4af862ac77be1116e537f975177e5
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-26 22:45:57 +0200

    Refactor app structure to integrate HomeComponent, consolidating hero, feature, and option sections

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroSectionComponent } from './../views/Home/components/hero-section/hero-section.component';
-import { FeatureSectionComponent } from '../views/Home/components/feature-section/feature-section.component';
-import { OptionSectionComponent } from '../views/Home/components/option-section/option-section.component';
+import { HomeComponent } from '../views/Home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeroSectionComponent,FeatureSectionComponent,OptionSectionComponent],
+  imports: [RouterOutlet,HomeComponent],
 templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
