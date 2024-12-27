ddad904ac9086705b3ead3beb89e330bd6b2db57
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-27 22:28:52 +0200

    Add Contact component with styles and integrate into app layout

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from '../views/Home/home.component';
 import { FooterComponent } from './../views/footer/footer.component';
 import { HeaderComponent } from './../views/header/header.component';
 import { OurFleetComponent } from '../views/our-fleet/our-fleet.component';
+import { ContactComponent } from '../views/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent,OurFleetComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent,OurFleetComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
