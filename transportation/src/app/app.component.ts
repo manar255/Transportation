45addf249b0a72b2f00807340b7ea157b32b1220
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-27 21:41:16 +0200

    Add Our Fleet component with styles and integrate into app layout

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../views/Home/home.component';
 import { FooterComponent } from './../views/footer/footer.component';
 import { HeaderComponent } from './../views/header/header.component';
+import { OurFleetComponent } from '../views/our-fleet/our-fleet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent,OurFleetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ transportation/src/views/our-fleet/our-fleet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-fleet',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-fleet.component.html',
+  styleUrl: './our-fleet.component.css'
+})
+export class OurFleetComponent {
+
+}
