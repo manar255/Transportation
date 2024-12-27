709b0f83a3494062e3da51af079827307117e933
Manar Khaled <94627280+manar255@users.noreply.github.com>
2024-12-27 20:50:14 +0200

    Add header and footer components, update styles, and refine hero section layout

--- transportation/src/app/app.component.ts
+++ transportation/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../views/Home/home.component';
 import { FooterComponent } from './../views/footer/footer.component';
+import { HeaderComponent } from './../views/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
